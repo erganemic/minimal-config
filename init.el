179b352fbafce3937fbeab1abd0c8dbf8257dc06
John Mathena <jmmathena@gmail.com>
2021-10-27 11:35:38 -0500

    Minimal config for reproduction

---
+++ init.el
@@ -0,0 +1,16 @@
+;; -*- lexical-binding: t; -*-
+(setq straight-check-for-modifications '(check-on-save find-when-checking)
+      straight-repository-branch "develop")
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
---
+++ straight/versions/default.el
@@ -0,0 +1,6 @@
+(("el-get" . "b5a5a405d04f61ec9c5fcb19357a50a4b9e36a25")
+ ("emacsmirror-mirror" . "5f3b55a1511cb481db1c715fb9fc6b82fbe5f56d")
+ ("gnu-elpa-mirror" . "4ccd8364a6d796f0c0781dd79adfd86231de21ad")
+ ("melpa" . "cdd8115e3ab3df5f74a21dbf63d89ee11b4f1c17")
+ ("straight.el" . "c17b5737456c10ecaaa9f88a9f6c03ca17212fdf"))
+:beta
